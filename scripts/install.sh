cf8dface09f2301c05b3556c243c6acb18567b60
David Linhardt <divad2012@web.de>
2025-03-26 23:31:50 +0100

    add inkscape to installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,7 +27,7 @@ spinner $!
 
 # APT install
 progress_message "Installing required APT packages"
-sudo apt install -y texlive-full pandoc biber lmodern imagemagick flatpak 2>&1 | tee install.log &
+sudo apt install -y texlive-full pandoc biber lmodern imagemagick flatpak inkscape 2>&1 | tee install.log &
 spinner $!
 
 # flatpak install
